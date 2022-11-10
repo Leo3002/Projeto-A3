8e10f4fc48412ae452495f32dbcd2c323844c6f0
toraotoreto <111095126+toraotoreto@users.noreply.github.com>
2022-11-10 20:25:53 -0300

    Add files via upload

---
+++ build/classes/Gerenciamento.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.a31;
+
+/**
+ *
+ * @author Alunos
+ */
+public class Gerenciamento {
+    private String status;
+    
+    
+    
+    
+    public String getStatus(){
+        return status;
+        
+    }
+    
+    public void setStatus(String Status){
+        this.status = status;
+        
+    }
+    
+}
