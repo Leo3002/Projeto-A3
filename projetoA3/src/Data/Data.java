826a900361b29a8d15aead03693cf4d55334d052
toraotoreto <111095126+toraotoreto@users.noreply.github.com>
2022-11-21 20:56:55 -0300

    Add files via upload
    Projeto final, basta apenas completar o codigo e o design

---
+++ projetoA3/src/Data/Data.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Data;
+
+/**
+ *
+ * @author gabri
+ */
+public class Data {
+    private String Data;
+    private double HorarioEntrada;
+    private double HorarioSaida;
+    
+    public String getData(){
+        return Data;
+    }
+    
+    public void setData(String Data){
+        this.Data = Data;
+    }
+    
+    public double getHorarioEntrada(){
+        return HorarioEntrada;
+    }
+    
+    public void setHorarioEntrada(double HorarioSaida){
+        this.HorarioEntrada = HorarioEntrada;
+    }
+    
+    public double getHorarioSaida(){
+        return HorarioSaida;
+    }
+    
+    public void setHorarioSaida(double HorarioSaida){
+        this.HorarioSaida = HorarioSaida;
+    }
+    
+}
---
+++ projetoA3/src/Projeto/Projeto.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Projeto;
+
+/**
+ *
+ * @author gabri
+ */
+public class Projeto {
+    
+     private String NomeProjeto;
+    private int IDProjeto;
+    
+    public String getNomeProjeto() {
+        return NomeProjeto;
+    }
+
+    public void setNomeProjeto(String NomeProjeto) {
+        this.NomeProjeto = NomeProjeto;
+    }
+
+    public int getIDProjeto() {
+        return IDProjeto;
+    }
+
+    public void setIDProjeto(int IDProjeto) {
+        this.IDProjeto = IDProjeto;
+    }
+    
+}
